beb3dfc7109bf6fb3920635b5485dc7b1726fc32
sayef <sayef@tigerit.com>
2020-10-09 00:38:48 +0200

    Change file path from relative to absoulte.

--- coref_ops.py
+++ coref_ops.py
@@ -2,10 +2,13 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import os
 import tensorflow as tf
 from tensorflow.python import pywrap_tensorflow
 
-coref_op_library = tf.load_op_library("./coref_kernels.so")
+_dir = os.path.dirname(os.path.abspath(__file__))
+
+coref_op_library = tf.load_op_library(os.path.join(_dir, "coref_kernels.so"))
 
 extract_spans = coref_op_library.extract_spans
 tf.NotDifferentiable("ExtractSpans")
